36640ca31dda38c2e8f2a29a356ebd99c8f0ce63
Mickael42 <mickael.wala@gmail.com>
2019-06-20 15:57:07 +0200

    update

--- htdocs/pages/main.php
+++ htdocs/pages/main.php
@@ -9,14 +9,14 @@
         <h1>Bienvenue chez OneValue !</h1>
         <p>Tempor est quis eu nostrud laborum nostrud culpa. Non labore laboris elit ea minim sit non esse ullamco tempor officia ad duis. Ea est tempor reprehenderit ullamco sint ullamco. Aliquip aute cupidatat deserunt incididunt minim.</p>
         <div class="button-head-main">
-            <button type="button" class="btn btn-secondary button-call-to-action">REJOINGNEZ NOUS ! </button>
+            <button type="button" class="btn btn-secondary justify-content-center">REJOINGNEZ NOUS ! </button>
         </div>
     </div>
 
     <div class="container-fluid">
         <div class="row">
             <div class="col-8 col-sm-8 content-details">
-            <h2 class="title-content-details">Lorem Ipsum</h2>
+            <h2 class="d-flex justify-content-center title-content-details">Lorem Ipsum</h2>
 
             <div class="row content-details-col">
                 <div class="col">
