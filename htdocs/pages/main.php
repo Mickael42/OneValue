9558a3bee8f29c574ca9a63f40098d2e380ccf34
Mickael42 <mickael.wala@gmail.com>
2019-06-20 10:52:01 +0200

    update

---
+++ htdocs/pages/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<?php include('../partials/head.php') ?>
+<body>
+    <div class="head-main-page">
+        <h1>
+    </div>
+
+
+
+
+
+<?php include('../partials.scripts.php') ?>
+</body>
+</html>
