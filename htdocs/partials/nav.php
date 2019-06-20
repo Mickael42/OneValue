ccfaa5d73fbbe5174f0c90af7c4b69bbf05452b9
InovaXion <mmghaoues@gmail.com>
2019-06-20 11:24:19 +0200

    update

---
+++ htdocs/partials/nav.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+<img id="nav-logo" src="../assets/img/logo-onevalue.png" alt="Logo OneValue">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div style="padding-top:15px;" class="collapse navbar-collapse align-right" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="#">ACCUEIL <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">AGENCE WEB</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CENTRE DE FORMATION</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">A PROPOS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ACCOMPAGNEMENT</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CONTACT</a>
+      </li>
+    </ul>
+  </div>
+</nav>
