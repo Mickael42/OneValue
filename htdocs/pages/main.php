57638a3bb9d9a9988def25f8d7cd54c3616643e9
Mickael42 <mickael.wala@gmail.com>
2019-06-20 11:24:48 +0200

    update

--- htdocs/pages/main.php
+++ htdocs/pages/main.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="Fr">
 <?php include('../partials/head.php') ?>
+
 <body>
     <div class="head-main-page">
-        <h1>Bienvenue chez OneValue !<h1>
-            <p>Ut sit elit ipsum est esse sit mollit ad sunt cillum proident laboris eu quis.</p>
-            <div class="button-head-main">
-            <button type="button" class="btn btn-secondary">Test boutton </button>
-            </div>
+        <h1>Bienvenue chez OneValue !</h1>
+        <p>Ut sit elit ipsum est esse sit mollit ad sunt cillum proident laboris eu quis.</p>
+                <div class="button-head-main">
+                    <button type="button" class="btn btn-secondary">Rejoignez-nous </button>
+                </div>
     </div>
 
+<div></div>
 
 
 
-
-<?php include('../partials/footer.php') ?>
+    <?php include('../partials/footer.php') ?>
 </body>
 <?php include('../partials/scripts.php') ?>
+
 </html>
