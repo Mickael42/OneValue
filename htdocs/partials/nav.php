9c6ece53b4b120a24a2433008ecf2188dcbf882f
InovaXion <mmghaoues@gmail.com>
2019-06-20 15:27:52 +0200

    update

--- htdocs/partials/nav.php
+++ htdocs/partials/nav.php
@@ -1,6 +1,6 @@
 <nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <div id="logo-bg">
-        <a class="navbar-brand" href="#"> <img class="align-items-center" id="logo-nav" src="../assets/img/logo-onevalue.png" alt=""> </a>
+        <a class="navbar-brand" href="#"> <img class="align-item onglets-center" id="logo-nav" src="../assets/img/logo-onevalue.png" alt=""> </a>
     </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,22 +8,22 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         
-            <div class="nav-item">
+            <div class="nav-item onglet">
                 <a class="nav-link" href="#">ACCUEIL</a>
             </div>
-            <div class="nav-item">
+            <div class="nav-item onglet">
                 <a class="nav-link" href="#">AGENCE WEB</a>
             </div>
-            <div class="nav-item">
+            <div class="nav-item onglet">
                 <a class="nav-link" href="#">CENTRE DE FORMATION</a>
             </div>
-            <div class="nav-item">
+            <div class="nav-item onglet">
                 <a class="nav-link" href="#">A PROPOS</a>
             </div>
-            <div class="nav-item">
+            <div class="nav-item onglet">
                 <a class="nav-link" href="#">ACCOMAPGNEMENT</a>
             </div>
-            <div class="nav-item">
+            <div class="nav-item onglet">
                 <a class="nav-link" href="#">CONTACT</a>
             </div>
 
