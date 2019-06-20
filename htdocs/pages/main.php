ba8e3b80efb0349ff6cf359ced863cb94d4a7fb4
Mickael42 <mickael.wala@gmail.com>
2019-06-20 12:09:08 +0200

    update

--- htdocs/pages/main.php
+++ htdocs/pages/main.php
@@ -6,25 +6,44 @@
     <div class="head-main-page">
         <h1>Bienvenue chez OneValue !</h1>
         <p>Ut sit elit ipsum est esse sit mollit ad sunt cillum proident laboris eu quis.</p>
-                <div class="button-head-main">
-                    <button type="button" class="btn btn-secondary">Rejoignez-nous </button>
-                </div>
+        <div class="button-head-main">
+            <button type="button" class="btn btn-secondary">Rejoignez-nous </button>
+        </div>
     </div>
 
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-8 col-sm-8">
-            <div class="content-details">
-                <img src="https://picsum.photos/200/200" alt="">
-                <p>Est elit adipisicing commodo ut proident reprehenderit velit pariatur adipisicing. Sint aliquip fugiat ullamco consectetur mollit ea commodo eu est dolor voluptate irure sunt. Deserunt sunt fugiat nulla laboris ea elit. Cupidatat voluptate nisi pariatur fugiat anim aliquip voluptate incididunt eu dolor. Ipsum incididunt nulla anim sint id duis velit esse.</p>
-
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-8 col-sm-8 col-content-details">
+                <div class=" row content-details">
+                    <div class="col-4 col-sm-4">
+                        <img src="https://picsum.photos/200/200" alt="">
+                    </div>
+                    <div class="col-8 col-sm-8 content-text">
+                        <p>Est elit adipisicing commodo ut proident reprehenderit velit pariatur adipisicing. Sint aliquip fugiat ullamco consectetur mollit ea commodo eu est dolor voluptate irure sunt. Deserunt sunt fugiat nulla laboris ea elit. Cupidatat voluptate nisi pariatur fugiat anim aliquip voluptate incididunt eu dolor.</p>
+                    </div>
+                </div>
+                <div class=" row content-details">
+                    <div class="col-4 col-sm-4">
+                        <img src="https://picsum.photos/200/200" alt="">
+                    </div>
+                    <div class="col-8 col-sm-8 content-text">
+                        <p>Est elit adipisicing commodo ut proident reprehenderit velit pariatur adipisicing. Sint aliquip fugiat ullamco consectetur mollit ea commodo eu est dolor voluptate irure sunt. Deserunt sunt fugiat nulla laboris ea elit. Cupidatat voluptate nisi pariatur fugiat anim aliquip voluptate incididunt eu dolor.</p>
+                    </div>
+                </div>
+                <div class=" row content-details">
+                    <div class="col-4 col-sm-4">
+                        <img src="https://picsum.photos/200/200" alt="">
+                    </div>
+                    <div class="col-8 col-sm-8 content-text">
+                        <p>Est elit adipisicing commodo ut proident reprehenderit velit pariatur adipisicing. Sint aliquip fugiat ullamco consectetur mollit ea commodo eu est dolor voluptate irure sunt. Deserunt sunt fugiat nulla laboris ea elit. Cupidatat voluptate nisi pariatur fugiat anim aliquip voluptate incididunt eu dolor.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 col-sm-4 ">
+                <p>Labore nisi eu ullamco do voluptate labore nisi laborum incididunt ex aliqua. Enim sunt commodo commodo veniam. Ut ex Lorem exercitation consectetur enim sunt ad proident incididunt incididunt non ipsum.</p>
             </div>
         </div>
-        <div class="col-4 col-sm-4">
-            <p>Labore nisi eu ullamco do voluptate labore nisi laborum incididunt ex aliqua. Enim sunt commodo commodo veniam. Ut ex Lorem exercitation consectetur enim sunt ad proident incididunt incididunt non ipsum.</p>
-        </div>
     </div>
-</div>
 
 
 
