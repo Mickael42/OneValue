985b82473d1bb64b4223ce39646156f0065c652d
Mickael42 <mickael.wala@gmail.com>
2019-06-21 10:42:04 +0200

    update

--- htdocs/pages/contact.php
+++ htdocs/pages/contact.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="Fr">
 <?php include('../partials/head.php') ?>
+
 <body>
-    
-<?php include('../partials/nav.php') ?>
+    <?php include('../partials/nav.php') ?>
 
 
 
 
+
+    <?php include('../partials/footer.php') ?>
 </body>
-
-
-<?php include('../partials/footer.php') ?>
 <?php include('../partials/scripts.php') ?>
+
 </html>
