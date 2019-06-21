8753940822d217284b0d9a7871a105a04c0edd1b
Mickael42 <mickael.wala@gmail.com>
2019-06-21 09:33:37 +0200

    update

--- htdocs/partials/nav.php
+++ htdocs/partials/nav.php
@@ -9,23 +9,23 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
         <div class="nav-item onglet">
-            <a class="nav-link" href="#">ACCUEIL</a>
+            <a class="nav-link" href="../pages/main.php">ACCUEIL</a>
         </div>
         <div class="nav-item onglet dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SOLUTIONS</a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">AGENCE WEB</a>
-                <a class="dropdown-item" href="#">OFFRE DIGITAL</a>
+                <a class="dropdown-item" href="../pages/agenceWeb.php">AGENCE WEB</a>
+                <a class="dropdown-item" href="../pages/offreDigital.php">OFFRE DIGITAL</a>
             </div>
         </div>
         <div class="nav-item onglet">
-            <a class="nav-link" href="#">CENTRE DE FORMATION</a>
+            <a class="nav-link" href="../pages/centreDeFormation.php">CENTRE DE FORMATION</a>
         </div>
         <div class="nav-item onglet">
-            <a class="nav-link" href="#">A PROPOS</a>
+            <a class="nav-link" href="../pages/aPropos.php">A PROPOS</a>
         </div>
         <div class="nav-item onglet">
-            <a class="nav-link" href="#">CONTACT</a>
+            <a class="nav-link" href="../pages/contact.php">CONTACT</a>
         </div>
 
     </div>
