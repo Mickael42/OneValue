c9d437d25e21b3c51b0384b8c335b609e0923486
InovaXion <mmghaoues@gmail.com>
2019-06-21 10:14:45 +0200

    update du logo blanc !

--- htdocs/partials/head.php
+++ htdocs/partials/head.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">  
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/header.css">
+    <link rel="stylesheet" href="../assets/css/agenceWeb.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet"> 
 </head>
